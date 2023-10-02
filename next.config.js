79056a3cfe712dc01ed9c606301f8daa22fd0b6b
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-10-02 13:19:52 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 module.exports = {
     basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
     trailingSlash: !!process.env.NEXT_PUBLIC_BASE_PATH,
-    i18n: {
-        defaultLocale: 'fr',
-        locales: ['fr', 'en'],
-    },
+
 };
